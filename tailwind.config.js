53d072a1e1580bcdc2450a2342b0e3b512e2dce8
KarlGilden <karl.gildenhuys@gmail.com>
2022-08-29 18:50:10 +1200

    add sections and update style to match wireframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx}"],
   theme: {
-    extend: {},
+    colors: {
+      'black': "#000",
+      'white': "#fff",
+      'offwhite': "#EDEDED",
+      'primary': "#5384E4",
+      'secondary': "#222",
+      'transparent': 'transparent'
+    },
+    fontFamily: {
+      'title': ['Alegreya\\ Sans', 'sans-serif']
+    }
   },
   plugins: [],
 }
